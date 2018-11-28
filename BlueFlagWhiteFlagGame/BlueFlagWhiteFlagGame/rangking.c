051101d1d9243e778ac0514136cc88209931e042
KimKyungHoon <Byul2sol@naver.com>
2018-11-28 11:55:07 +0900

    ranking system complete

--- BlueFlagWhiteFlagGame/BlueFlagWhiteFlagGame/rangking.c
+++ BlueFlagWhiteFlagGame/BlueFlagWhiteFlagGame/rangking.c
@@ -3,21 +3,25 @@
 #include <stdlib.h>
 #include "console.h"
 
-#define BUFFER 2000
+#define BUFFER 1000
 void showRangking()
 {
 	char buffer[BUFFER] = "";
 	FILE *fp;
-	char *eachData[10] = { "", "", "", "", "", "", "", "", "" ,"" }; // 데이터 초기화
-	char *eachTime[10] = { "", "", "", "", "", "", "", "", "" ,"" }; // SORT를 위해 시간만 따로 분리
+	char *eachData[BUFFER];
+	for (int i = 0;i < BUFFER;i++)
+	{
+		eachData[i] = "";
+	} // 모든 데이터 초기화
 	int rangkingIndex = 0;
+	int inputMax = 0; // 랭킹을 출력하는 최대 수를 지정.
 	struct _recordData {
 		int rank;
 		char name[10];
-	    double playTime;
-	}recordData[10];
+		double playTime;
+	}recordData[BUFFER];
 
-	fopen_s(&fp,"data\\gamedata.txt", "r");
+	fopen_s(&fp, "data\\gamedata.txt", "r");
 	if (fp == NULL)
 	{
 		gotoxy(22, 15); printf("출력할 데이터가 없습니다. \n");
@@ -36,15 +40,16 @@ void showRangking()
 			rangkingIndex++;
 		}
 
-		
-		for (int i = 0;i < rangkingIndex; i++) 
+
+		for (int i = 0;i < rangkingIndex; i++)
 		{
 			recordData[i].rank = 1;
 			char *timeToken = strtok(eachData[i], " ");
 			strcpy(recordData[i].name, timeToken);
 			timeToken = strtok(NULL, "");
 			recordData[i].playTime = atof(timeToken);// 각 데이터들을 토큰으로 분할하는 과정		
-		} 
+		}
+
 
 		for (int i = 0; i < rangkingIndex; i++)
 		{
@@ -55,16 +60,20 @@ void showRangking()
 					recordData[i].rank++;
 				}
 			}
-			gotoxy(3, 3 + recordData[i].rank); printf("%d", recordData[i].rank);
-			printf(" %s   %.2f", recordData[i].name, recordData[i].playTime);
-		}
-		
-		
-		
+			/*gotoxy(3, 3 + recordData[i].rank); printf("%d", recordData[i].rank);
+			printf(" %s   %.2f", recordData[i].name, recordData[i].playTime);*/
+		} // 각 데이터들의 순위를 매기는 알고리즘
+		gotoxy(26, 26);printf("TOTAL DATA 수 : %d", rangkingIndex);
 
+		if (rangkingIndex <= 20) inputMax = rangkingIndex; // 20개 이하가 입력되있다면 최대 출력은 랭킹의 인덱스까지만 출력. 
+		else inputMax = 20; // 20개 이상이 입력되있다면 최대 20개까지만 출력.
 		for (int i = 0; i < rangkingIndex; i++)
 		{
-			
+			if (recordData[i].rank <= 20)
+			{
+				gotoxy(3, 3 + recordData[i].rank); printf("%d", recordData[i].rank);
+				printf(" %s   %.2f", recordData[i].name, recordData[i].playTime);
+			}
 		}
 
 		fclose(fp);
--- BlueFlagWhiteFlagGame/BlueFlagWhiteFlagGame/rangking.h
+++ BlueFlagWhiteFlagGame/BlueFlagWhiteFlagGame/rangking.h
@@ -1,6 +1,5 @@
 #ifndef __RANGKING_H__
 void showRangking();
-double _atoi(char *_time[10]);
 #define __RANKING_H__
 
 #endif
